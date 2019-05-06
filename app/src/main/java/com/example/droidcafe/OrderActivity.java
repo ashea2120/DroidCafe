932d5ffdc94e82002d38aff49c9f1c0193ad1bb0
ashea2120 <ashea2120@yahoo.com>
2019-05-05 21:23:48 -0400

    added actions to the newly added menuitem icons. The shopping cart icon launches the OrderActivity

--- app/src/main/java/com/example/droidcafe/OrderActivity.java
+++ app/src/main/java/com/example/droidcafe/OrderActivity.java
@@ -3,6 +3,7 @@ package com.example.droidcafe;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
